228bb5a1b0bec593d6420b502e265249575ea821
jmX86 <jm54307@fer.hr>
2023-11-07 15:45:25 +0100

    Dodao povezivanje na WiFi mrezu i include za library PubSubClient koju bi koristili za MQTT komunikaciju

--- AquaObserver-esp32-wifi/AquaObserver-esp32-wifi.ino
+++ AquaObserver-esp32-wifi/AquaObserver-esp32-wifi.ino
@@ -1,5 +1,21 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid       = "YOUR_SSID";
+const char* password   = "YOUR_PASS";
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  //connect to WiFi
+  Serial.printf("Connecting to %s ", ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+  }
+  Serial.println(" CONNECTED");
 
 }
 
